2126a4d0f035f6103d203896e2b3f277a69b073d
Joel <joelekwegh3@gmail.com>
2023-12-06 00:49:51 +0100

    spring security

---
+++ src/main/java/com/example/School/Configurations/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.example.School.Configurations;public class SecurityConfig {
+}
--- src/main/java/com/example/School/Repositories/FeedbackRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.School.Repositories;
-import com.example.School.Entities.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
-
-}
